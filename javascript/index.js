bb66bb3b669ae78314eed41ac889da45395a87b1
Ghaith Ali <ghaithammar2008@gmail.com>
2016-09-29 19:46:00 +0200

    added basic javascript 4digits checking

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,23 @@
 /**
  * Created by GhaithAli on 28/09/16.
  */
+$(document).ready(function(){
+    var searchInputBox = $(".searchInput"),
+        searchButton = $(".searchButton"),
+        postCodeInfo = $(".postCodeInfo");
+
+    searchInputBox.on( "click" , function(){
+            searchInputBox.val("");
+    });
+
+    searchButton.on("click" , function (){
+        var postNumber = searchInputBox.val(),
+            regExp4digits = /^\d{4}$/;
+        if (regExp4digits.test(postNumber)){
+            postCodeInfo.text("");
+            //todo: code to go to the next page (the list page).
+        } else {
+            postCodeInfo.text("Insert a valid post number");
+        }
+    });
+});
